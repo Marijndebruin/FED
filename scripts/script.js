a35f3ec7ba8f097ab954c32717a41e22c267361d
Marijndebruin <94321415+Marijndebruin@users.noreply.github.com>
2021-11-25 21:10:22 +0100

    Update 1.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+function hamburger() {
+        window.addEventListener("resize", function() {
+            if (window.matchMedia("(min-width: 375px)").matches) {
+              console.log("Screen width is at least 500px")
+            } else {
+            document.getElementById("navtest").style.display = "none";
+            }
+          })
+    }
